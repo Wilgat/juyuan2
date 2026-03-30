e6d4d03ae4f90b933b438e81056381cd22895a7c
Wilgat Wong <wilgat.wong@gmail.com>
2026-03-30 13:45:44 +0800

    Update MessageUtils

--- juyuan-common/src/main/java/com/ruoyi/common/utils/MessageUtils.java
+++ juyuan-common/src/main/java/com/ruoyi/common/utils/MessageUtils.java
@@ -1,32 +1,27 @@
-/*    */ package com.ruoyi.common.utils;
-/*    */ 
-/*    */ import com.ruoyi.common.utils.spring.SpringUtils;
-/*    */ import org.springframework.context.MessageSource;
-/*    */ import org.springframework.context.i18n.LocaleContextHolder;
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ 
-/*    */ public class MessageUtils
-/*    */ {
-/*    */   public static String message(String code, Object... args) {
-/* 23 */     MessageSource messageSource = (MessageSource)SpringUtils.getBean(MessageSource.class);
-/* 24 */     return messageSource.getMessage(code, args, LocaleContextHolder.getLocale());
-/*    */   }
-/*    */ }
+package com.ruoyi.common.utils;
 
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import com.ruoyi.common.utils.spring.SpringUtils;
 
-/* Location:              /home/adm01/Music/juyuan-common-3.8.6/!/com/ruoyi/common/utils/MessageUtils.class
- * Java compiler version: 8 (52.0)
- * JD-Core Version:       1.1.3
+/**
+ * 获取i18n资源文件
+ * 
+ * @author ruoyi
  */
+public class MessageUtils
+{
+    /**
+     * 根据消息键和参数 获取消息 委托给spring messageSource
+     *
+     * @param code 消息键
+     * @param args 参数
+     * @return 获取国际化翻译值
+     */
+    public static String message(String code, Object... args)
+    {
+        MessageSource messageSource = SpringUtils.getBean(MessageSource.class);
+        return messageSource.getMessage(code, args, LocaleContextHolder.getLocale());
+    }
+}
+// aligned
